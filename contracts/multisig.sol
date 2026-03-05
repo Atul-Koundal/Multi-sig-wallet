28db28edea0799b5d9e8832122acff8a0e2b301f
Atul-Koundal <24bcs031@nith.ac.in>
2026-03-05 15:17:27 +0530

    Fixed some of the solidity part

--- contracts/multisig.sol
+++ contracts/multisig.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "./interfaces/IMultiSigWallet.sol";
+import "./interfaces/imultisig.sol";
 
 /**
  * @title  MultiSigWallet
--- contracts/multisigfac.sol
+++ contracts/multisigfac.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "./MultiSigWallet.sol";
+import "./multisig.sol";
 
 /**
  * @title  MultiSigWalletFactory
